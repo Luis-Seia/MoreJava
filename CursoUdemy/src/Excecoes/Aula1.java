63bfbfb278e892e49339f374cb68c78bf50b7bb1
Luis Seia <luis.seiax@gmail.com>
2022-05-08 22:15:05 +0200

    hehey

--- CursoUdemy/src/Excecoes/Aula1.java
+++ CursoUdemy/src/Excecoes/Aula1.java
@@ -6,10 +6,11 @@ public class Aula1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		System.out.println("sfsdfg");
 		String[] vet = scan.nextLine().split(" ");
 		int pos = scan.nextInt();
 		System.out.println(vet[pos]);
+		scan.close();
 
 	}
 
