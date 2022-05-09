2c86086961d3e6ba338c6444d1f41621ddd4eaad
Luis Seia <luis.seiax@gmail.com>
2022-05-09 20:36:16 +0200

    exception

---
+++ CursoUdemy/src/Excecoes/ruim/program.java
@@ -0,0 +1,17 @@
+package Excecoes.ruim;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	
+
+	}
+
+}
