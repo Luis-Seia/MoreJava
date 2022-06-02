075f40c2bd4112432ca05048d81b550c44d1815c
Luis Seia <luis.seiax@gmail.com>
2022-06-02 23:23:26 +0200

    Update Lista.java

--- Activities/src/arrays/Lista.java
+++ Activities/src/arrays/Lista.java
@@ -21,7 +21,7 @@ public class Lista {
 		}
 		lista.removeIf(x -> x.charAt(0)== 'M'); // remove o elemento que comeca com o caracter indicado
 		
-		lista.indexOf("luis"); // retorne a posicao da string na lista
+		
 		
 		List<String> result = lista .stream().filter(x -> x.charAt(0)== 'A').collect(Collectors.toList());// filtra os lementos qu comecam com o caracter indicado
 		String name = lista.stream().filter(x -> x.charAt(0)== 'A').findFirst().orElse(null); // retorna a primeira String com o caracter imdicado
